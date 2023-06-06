3f80901d1c473174c1ade3d1003cf6386e9bb3a2
rm86539 <rm86539@estudante.fieb.edu.br>
2023-06-06 08:30:10 -0300

    balonismo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Aula de Orientação á Objeto
+
+        // Declarar Objetos
+        Pessoa adao;
+        // classe "Pessoa" que deu origem ao objeto: "Adão"
+
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+
+        //Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+        // Acionar comportamento - fazer o adao falar
+        adao.falar();
+        eva.falar();
     }
 }
---
+++ workspace/Balonismo2F/src/Balao.java
@@ -0,0 +1,18 @@
+public class Balao {
+
+    // Atributos
+    long id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+
+    // Métodos
+    public String voar(){
+        return "voando";
+    }
+
+}
